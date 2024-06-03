9f3f925c90ea24007f99570bdbd6ddd278665821
Titouan Launay <titouan.launay@lleedpartners.com>
2024-06-03 13:06:01 +0200

    Adds client testing

---
+++ packages/typescript/src/test/esm.test.ts
@@ -0,0 +1,35 @@
+import { FileforgeClient } from "../../dist/esm/index";
+import { describe, it, expect } from "vitest";
+import * as fs from "node:fs";
+
+const NODE_VERSION = parseInt(process.versions.node.split(".")[0]);
+
+describe("FileforgeClient", () => {
+  it.skipIf(NODE_VERSION < 20)("should take File", async () => {
+    const ff = new FileforgeClient({
+      apiKey: process.env.FILEFORGE_API_KEY,
+    });
+
+    const response = await ff.pdf.form.mark(
+      new File([fs.readFileSync(__dirname + "/samples/form.pdf")], "form.pdf", {
+        type: "application/pdf",
+      }),
+      { options: {} },
+    );
+
+    expect(response).toBeDefined();
+  });
+
+  it("should take fs.createReadStream", async () => {
+    const ff = new FileforgeClient({
+      apiKey: process.env.FILEFORGE_API_KEY,
+    });
+
+    const response = await ff.pdf.form.mark(
+      fs.createReadStream(__dirname + "/samples/form.pdf"),
+      {},
+    );
+
+    expect(response).toBeDefined();
+  });
+});
