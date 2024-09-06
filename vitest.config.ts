ec484a9dd317f903422bb00dfab5e6760f9af9af
Titouan Launay <titouan.launay@lleedpartners.com>
2024-09-06 15:05:32 -0700

    Increases test timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    testTimeout: 15000,
     coverage: {
       provider: "v8",
       reporter: ["html", "json-summary"],
